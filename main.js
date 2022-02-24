a9f4dc694ac18494f7e6a7e16b0a27f4c3808bc5
Mario-Pet <mpetkovic691@gmail.com>
2022-02-24 10:46:13 +0100

    Added imgs, fixed structure, added theme, fixed language selector

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.querySelector("select").addEventListener("change", e => {
+    console.log(e.target.value)
     if (document.querySelector("select").value == "english") {
-        document.querySelector("#makiHeadline").innerHTML = 'hello, i am <span class="maki">maki;</span>';
+        document.querySelector("#makiHeadline").innerHTML = 'hello, i am <span class="maki" id="makiLine">maki;</span>';
         document.querySelector("#makiHeadline").classList.remove("makiHeadline")
         document.querySelector("#makiHeadline").classList.add("makiHeadlineEnglish")
         document.querySelector("#underHeadline").textContent = "I love to code and I love to learn."
@@ -8,14 +9,13 @@ document.querySelector("select").addEventListener("change", e => {
         document.querySelector("#projectsBankApp").textContent = "bank app;";
         document.querySelector("#projectsCinemaApp").textContent = "cinema seat booking app;";
         document.querySelector("#projectsCountdown").textContent = "countdown till new years;";
-        document.querySelector("#projectsTyperacer").textContent = "typeracer;";
         document.querySelector("#about").textContent = "ABOUT ME";
         document.querySelector("#contact").textContent = "CONTACT";
         document.querySelector("#aboutLine").textContent = "I often read books, play the piano and i love music.";
         document.querySelector("#secondSectionThirdColumn").classList.toggle("secondSectionThirdColumnEnglish")
 
     } else {
-        document.querySelector("#makiHeadline").innerHTML = 'hallo, ich bin <span class="maki">maki;</span>';
+        document.querySelector("#makiHeadline").innerHTML = 'hallo, ich bin <span class="maki" id="makiLine">maki;</span>';
         document.querySelector("#makiHeadline").classList.remove("makiHeadlineEnglish")
         document.querySelector("#makiHeadline").classList.add("makiHeadline")
         document.querySelector("#underHeadline").textContent = "Ich liebe zu programmieren und ich liebe zu lernen."
@@ -23,7 +23,6 @@ document.querySelector("select").addEventListener("change", e => {
         document.querySelector("#projectsBankApp").textContent = "Bank App;";
         document.querySelector("#projectsCinemaApp").textContent = "Kinoplatzreservierung;";
         document.querySelector("#projectsCountdown").textContent = "Countdown bis zum neuen Jahr;";
-        document.querySelector("#projectsTyperacer").textContent = "Typeracer;";
         document.querySelector("#about").textContent = "ÜBER MICH";
         document.querySelector("#contact").textContent = "KONTAKT";
         document.querySelector("#aboutLine").textContent = "Ich lese Bücher oft, spiele Klavier und liebe Musik";
@@ -33,19 +32,85 @@ document.querySelector("select").addEventListener("change", e => {
 })
 
 document.querySelector("#theme").addEventListener("click", e => {
+    const btn = document.querySelector("#theme")
+    if (btn.value == "white") {
+        btn.value = "dark";
+    } else {
+        btn.value = "white";
+    }
+
     if (e.target.value == "white") {
         document.querySelector("#body").classList.toggle("darkBG");
+
         document.querySelector("#makiHeadline").classList.toggle("white");
+        document.querySelector("#makiLine").classList.toggle("white");
         document.querySelector("#underHeadline").classList.toggle("white");
+
+        document.querySelector("#logoImg").setAttribute("src", "imgs/logoDark1080.png")
+
         document.querySelector("#projectsHeadline").classList.toggle("white");
         document.querySelector("#projectsBankApp").classList.toggle("white");
         document.querySelector("#projectsCinemaApp").classList.toggle("white");
         document.querySelector("#projectsCountdown").classList.toggle("white");
-        document.querySelector("#projectsTyperacer").classList.toggle("white");
+
         document.querySelector("#about").classList.toggle("white");
-        document.querySelector("#contact").classList.toggle("white");
         document.querySelector("#aboutLine").classList.toggle("white");
+
+        document.querySelector("#contact").classList.toggle("white");
         document.querySelector("#underContact").classList.toggle("white");
+
+        document.querySelector("#skillsLine").classList.toggle("white");
+        document.querySelector("#jsIcon").setAttribute("src", "imgs/skills/whbl/javascript.png")
+        document.querySelector("#htmlIcon").setAttribute("src", "imgs/skills/whbl/html.png")
+        document.querySelector("#cssIcon").setAttribute("src", "imgs/skills/whbl/css.png")
+        document.querySelector("#gitIcon").setAttribute("src", "imgs/skills/whbl/git.png")
+        document.querySelector("#githubIcon").setAttribute("src", "imgs/skills/whbl/github.png")
+        document.querySelector("#firebaseIcon").setAttribute("src", "imgs/skills/whbl/firebase.png")
+        document.querySelector("#figmaIcon").setAttribute("src", "imgs/skills/whbl/figma.png")
+        document.querySelector("#adobexdIcon").setAttribute("src", "imgs/skills/whbl/adobexd.png")
+        document.querySelector("#photoshopIcon").setAttribute("src", "imgs/skills/whbl/photoshop.png")
+        document.querySelector("#aftereffectsIcon").setAttribute("src", "imgs/skills/whbl/aftereffects.png")
+        document.querySelector("#codewarsIcon").setAttribute("src", "imgs/skills/whbl/codewars.png")
+        document.querySelector("#codecademyIcon").setAttribute("src", "imgs/skills/whbl/codecademy.png")
+
+        document.querySelector("#language").classList.toggle("languageWhite");
+
+    } else if (e.target.value == "dark") {
+        document.querySelector("#body").classList.toggle("darkBG");
+
+        document.querySelector("#makiHeadline").classList.toggle("white");
+        document.querySelector("#makiLine").classList.toggle("white");
+        document.querySelector("#underHeadline").classList.toggle("white");
+
+        document.querySelector("#logoImg").setAttribute("src", "imgs/logoWhite1080.png")
+
+        document.querySelector("#projectsHeadline").classList.toggle("white");
+        document.querySelector("#projectsBankApp").classList.toggle("white");
+        document.querySelector("#projectsCinemaApp").classList.toggle("white");
+        document.querySelector("#projectsCountdown").classList.toggle("white");
+
+        document.querySelector("#about").classList.toggle("white");
+        document.querySelector("#aboutLine").classList.toggle("white");
+
+        document.querySelector("#contact").classList.toggle("white");
+        document.querySelector("#underContact").classList.toggle("white");
+
+        document.querySelector("#skillsLine").classList.toggle("white");
+
+        document.querySelector("#jsIcon").setAttribute("src", "imgs/skills/blwh/javascript.png")
+        document.querySelector("#htmlIcon").setAttribute("src", "imgs/skills/blwh/html.png")
+        document.querySelector("#cssIcon").setAttribute("src", "imgs/skills/blwh/css.png")
+        document.querySelector("#gitIcon").setAttribute("src", "imgs/skills/blwh/git.png")
+        document.querySelector("#githubIcon").setAttribute("src", "imgs/skills/blwh/github.png")
+        document.querySelector("#firebaseIcon").setAttribute("src", "imgs/skills/blwh/firebase.png")
+        document.querySelector("#figmaIcon").setAttribute("src", "imgs/skills/blwh/figma.png")
+        document.querySelector("#adobexdIcon").setAttribute("src", "imgs/skills/blwh/adobexd.png")
+        document.querySelector("#photoshopIcon").setAttribute("src", "imgs/skills/blwh/photoshop.png")
+        document.querySelector("#aftereffectsIcon").setAttribute("src", "imgs/skills/blwh/adobeaftereffects.png")
+        document.querySelector("#codewarsIcon").setAttribute("src", "imgs/skills/blwh/codewars.png")
+        document.querySelector("#codecademyIcon").setAttribute("src", "imgs/skills/blwh/codecademy.png")
+
+        document.querySelector("#language").classList.toggle("languageWhite");
 
     }
 })
